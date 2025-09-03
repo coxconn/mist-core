97411b14be83fc8d4d41c5025f6661939b59fbd0
john <aeln@live.com>
2025-09-03 16:13:59 +0800

    。。

--- src/03 Framework/MistCore.Framework.EService/ESClientAsync.cs
+++ src/03 Framework/MistCore.Framework.EService/ESClientAsync.cs
@@ -17,54 +17,12 @@ namespace MistCore.Framework.EService
     /// </summary>
     public partial class ESClient
     {
-        private readonly static ILogger<ESClient> logger = (ILogger<ESClient>)GlobalConfiguration.ServiceLocator.Instance.GetService(typeof(ILogger<ESClient>));
-
-        private readonly static Dictionary<string, ElasticClient> _clients = new Dictionary<string, ElasticClient>();
-
-        protected ElasticClient _client;
-
-        private string _conn;
-        public string conn { get { return _conn; } }
-
-        public ESClient(string conn)
-        {
-            this._conn = conn;
-
-            _client = GetElasticClient(conn);
-        }
-
-        public ElasticClient GetElasticClient(string conn)
-        {
-            ElasticClient model = null;
-
-            lock (_clients)
-            {
-                if (_clients.TryGetValue(conn, out model))
-                {
-                    return model;
-                }
-
-                var uris = conn.Split(new[] { ',', '，' }, StringSplitOptions.RemoveEmptyEntries).Select(u => new Uri(u));
-                StaticConnectionPool pool = new StaticConnectionPool(uris);
-                ConnectionSettings setting = new ConnectionSettings(pool);
-#if DEBUG
-                setting.DisableDirectStreaming(true);
-#endif
-                _client = new ElasticClient(setting);
-
-                lock (_clients)
-                {
-                    _clients[conn] = _client;
-                    return _client;
-                }
-            }
-        }
 
         #region Check Index Exist
 
-        public bool IndexExist(string index)
+        public async Task<bool> IndexExistAsync(string index)
         {
-            return _client.IndexExists(index).Exists;
+            return (await _client.IndexExistsAsync(index)).Exists;
         }
         #endregion
 
@@ -73,9 +31,9 @@ namespace MistCore.Framework.EService
         /// 创建索引
         /// </summary>
         /// <returns></returns>
-        public ICreateIndexResponse CreateIndex(string index, Func<CreateIndexDescriptor, ICreateIndexRequest> selector = null)
+        public async Task<ICreateIndexResponse> CreateIndexAsync(string index, Func<CreateIndexDescriptor, ICreateIndexRequest> selector = null)
         {
-            var response = _client.CreateIndex(index, selector);
+            var response = await _client.CreateIndexAsync(index, selector);
             return response;
         }
         #endregion
@@ -85,9 +43,9 @@ namespace MistCore.Framework.EService
         /// CreateMap
         /// </summary>
         /// <returns></returns>
-        public IPutMappingResponse CreateMap<T>(Func<PutMappingDescriptor<T>, IPutMappingRequest> selector) where T : class
+        public async Task<IPutMappingResponse> CreateMapAsync<T>(Func<PutMappingDescriptor<T>, IPutMappingRequest> selector) where T : class
         {
-            var response = _client.Map<T>(selector);
+            var response = await _client.MapAsync<T>(selector);
             return response;
         }
 
@@ -99,13 +57,13 @@ namespace MistCore.Framework.EService
         /// <typeparam name="T"></typeparam>
         /// <param name="ts"></param>
         /// <returns></returns>
-        public IBulkResponse BulkIndex<T>(string index, string type, List<T> ts, bool log = false) where T : class
+        public async Task<IBulkResponse> BulkIndexAsync<T>(string index, string type, List<T> ts, bool log = false) where T : class
         {
             var query = new BulkDescriptor();
             var query1 = query.Index(index).Type(type).IndexMany(ts);
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.Bulk(b => query1);
+            var rsp = await _client.BulkAsync(b => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -119,9 +77,9 @@ namespace MistCore.Framework.EService
         /// <typeparam name="T"></typeparam>
         /// <param name="id"></param>
         /// <returns></returns>
-        public IDeleteResponse DeleteById<T>(string index, string type, DocumentPath<T> id, bool log = false) where T : class
+        public async Task<IDeleteResponse> DeleteByIdAsync<T>(string index, string type, DocumentPath<T> id, bool log = false) where T : class
         {
-            var rsp = _client.Delete(id, d => d.Index(index).Type(type));
+            var rsp = await _client.DeleteAsync(id, d => d.Index(index).Type(type));
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. {rsp.OriginalException}");
@@ -135,14 +93,14 @@ namespace MistCore.Framework.EService
         /// <typeparam name="T"></typeparam>
         /// <param name="selector"></param>
         /// <returns></returns>
-        public IDeleteByQueryResponse DeleteByQuery<T>(string index, string type, Func<DeleteByQueryDescriptor<T>, IDeleteByQueryRequest> selector, bool log = false) where T : class
+        public async Task<IDeleteByQueryResponse> DeleteByQueryAsync<T>(string index, string type, Func<DeleteByQueryDescriptor<T>, IDeleteByQueryRequest> selector, bool log = false) where T : class
         {
             var query = new DeleteByQueryDescriptor<T>(index);
             query.Type(type);
             var query1 = selector(query);
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.DeleteByQuery<T>(s => query1);
+            var rsp = await _client.DeleteByQueryAsync<T>(s => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -155,9 +113,9 @@ namespace MistCore.Framework.EService
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public IForceMergeResponse ForceMerge(Indices indices)
+        public async Task<IForceMergeResponse> ForceMergeAsync(Indices indices)
         {
-            return _client.ForceMerge(indices, q => q.MaxNumSegments(1));
+            return await _client.ForceMergeAsync(indices, q => q.MaxNumSegments(1));
         }
 
         /// <summary>
@@ -170,13 +128,13 @@ namespace MistCore.Framework.EService
         /// <param name="fid">匹配主键</param>
         /// <param name="fdoc">更新字段</param>
         /// <returns></returns>
-        public IBulkResponse UpdateMany<T>(string index, string type, List<T> ts, Func<T, Id> fid, Func<T, object> fdoc, bool log = false) where T : class
+        public async Task<IBulkResponse> UpdateManyAsync<T>(string index, string type, List<T> ts, Func<T, Id> fid, Func<T, object> fdoc, bool log = false) where T : class
         {
             var query = new BulkDescriptor();
             var query1 = query.Index(index).Type(type).UpdateMany<T, object>(ts, (ud, t) => ud.Id(fid(t)).Doc(fdoc(t)));
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.Bulk(b => query1);
+            var rsp = await _client.BulkAsync(b => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -188,13 +146,13 @@ namespace MistCore.Framework.EService
         /// 查询
         /// </summary>
         /// <returns></returns>
-        public ISearchResponse<T> Search<T>(Func<SearchDescriptor<T>, ISearchRequest> selector = null, bool log = false) where T : class
+        public async Task<ISearchResponse<T>> SearchAsync<T>(Func<SearchDescriptor<T>, ISearchRequest> selector = null, bool log = false) where T : class
         {
             var query = new SearchDescriptor<T>();
             var query1 = selector(query);
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.Search<T>(s => query1);
+            var rsp = await _client.SearchAsync<T>(s => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -202,53 +160,26 @@ namespace MistCore.Framework.EService
             return rsp;
         }
 
-        public string SerializeToString(object data, SerializationFormatting formatting = SerializationFormatting.Indented)
-        {
-            return _client.Serializer.SerializeToString(data, formatting);
-        }
     }
 
 
-    public partial class ESClient<T> : ESClient where T : class, ESIndex<T>, new()
+    public partial class ESClient<T>
     {
-        private readonly static ILogger<ESClient> logger = (ILogger<ESClient>)GlobalConfiguration.ServiceLocator.Instance.GetService(typeof(ILogger<ESClient>));
-
-        public ESClient(string conn) : base(conn)
-        {
-            //var def = default(T);
-
-            //#region 索引初始化逻辑
-            //if (!IndexExist())
-            //{
-            //    if (!CreateIndex())
-            //    {
-            //        logger.Error($"failed on create index: {def.Index}");
-            //        throw new Exception($"failed on create index: {def.Index}");
-            //    }
-            //    if (!CreateMap())
-            //    {
-            //        logger.Error($"failed on create map: {def.Index}.{def.Type}");
-            //        throw new Exception($"failed on create index: {def.Index}");
-            //    }
-            //}
-            //#endregion
-        }
-
         /// <summary>
         /// 检查索引
         /// </summary>
-        public void CheckIndex()
+        public async Task CheckIndexAsync()
         {
-            if (!IndexExist())
+            if (!await IndexExistAsync())
             {
-                var createresponse = CreateIndex();
+                var createresponse = await CreateIndexAsync();
                 if (!createresponse.IsValid)
                 {
                     var def = new T();
                     logger.LogError($"failed on create index: {def.Index}", createresponse.OriginalException);
                     throw new Exception($"failed on create index: {def.Index}");
                 }
-                var mapresponse = CreateMap();
+                var mapresponse = await CreateMapAsync();
                 if (!mapresponse.IsValid)
                 {
                     var def = new T();
@@ -258,34 +189,34 @@ namespace MistCore.Framework.EService
             }
         }
 
-        public bool IndexExist()
+        public async Task<bool> IndexExistAsync()
         {
             var index = new T();
-            return _client.IndexExists(index.Index).Exists;
+            return (await _client.IndexExistsAsync(index.Index)).Exists;
         }
 
-        public ICreateIndexResponse CreateIndex()
+        public async Task<ICreateIndexResponse> CreateIndexAsync()
         {
             var index = new T();
-            var response = _client.CreateIndex(index.Index, cid => index.CreateIndex(cid));
+            var response = await _client.CreateIndexAsync(index.Index, cid => index.CreateIndex(cid));
             return response;
         }
 
-        public IPutMappingResponse CreateMap()
+        public async Task<IPutMappingResponse> CreateMapAsync()
         {
             var index = new T();
-            var response = _client.Map<T>(m => index.Map(m.Index(index.Index).Type(index.Type)));
+            var response = await _client.MapAsync<T>(m => index.Map(m.Index(index.Index).Type(index.Type)));
             return response;
         }
 
-        public IBulkResponse BulkIndex(List<T> ts, bool log = false)
+        public async Task<IBulkResponse> BulkIndexAsync(List<T> ts, bool log = false)
         {
             var index = new T();
             var query = new BulkDescriptor();
             var query1 = query.Index(index.Index).Type(index.Type).IndexMany(ts);
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.Bulk(b => query1);
+            var rsp = await _client.BulkAsync(b => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -293,10 +224,10 @@ namespace MistCore.Framework.EService
             return rsp;
         }
 
-        public IDeleteResponse DeleteById(string id, bool log = false)
+        public async Task<IDeleteResponse> DeleteByIdAsync(string id, bool log = false)
         {
             var index = new T();
-            var rsp = _client.Delete<T>(id, d => d.Index(index.Index).Type(index.Type));
+            var rsp = await _client.DeleteAsync<T>(id, d => d.Index(index.Index).Type(index.Type));
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. {rsp.OriginalException}");
@@ -304,7 +235,7 @@ namespace MistCore.Framework.EService
             return rsp;
         }
 
-        public IDeleteByQueryResponse DeleteByQuery(Func<DeleteByQueryDescriptor<T>, IDeleteByQueryRequest> selector, bool log = false)
+        public async Task<IDeleteByQueryResponse> DeleteByQueryAsync(Func<DeleteByQueryDescriptor<T>, IDeleteByQueryRequest> selector, bool log = false)
         {
             var index = new T();
             var query = new DeleteByQueryDescriptor<T>(index.Index);
@@ -312,7 +243,7 @@ namespace MistCore.Framework.EService
             var query1 = selector(query);
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.DeleteByQuery<T>(s => query1);
+            var rsp = await _client.DeleteByQueryAsync<T>(s => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -320,21 +251,21 @@ namespace MistCore.Framework.EService
             return rsp;
         }
 
-        public IForceMergeResponse ForceMerge()
+        public async Task<IForceMergeResponse> ForceMergeAsync()
         {
             var index = new T();
             //return _client.ForceMerge(Index, q => q.OnlyExpungeDeletes(true).MaxNumSegments(1));
-            return _client.ForceMerge(index.Index, q => q.MaxNumSegments(1));
+            return await _client.ForceMergeAsync(index.Index, q => q.MaxNumSegments(1));
         }
 
-        public IBulkResponse UpdateMany(List<T> ts, Func<T, Id> fid, Func<T, object> fdoc, bool log = false)
+        public async Task<IBulkResponse> UpdateManyAsync(List<T> ts, Func<T, Id> fid, Func<T, object> fdoc, bool log = false)
         {
             var index = new T();
             var query = new BulkDescriptor();
             var query1 = query.Index(index.Index).Type(index.Type).UpdateMany<T, object>(ts, (ud, t) => ud.Id(fid(t)).Doc(fdoc(t)));
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.Bulk(b => query1);
+            var rsp = await _client.BulkAsync(b => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -342,7 +273,7 @@ namespace MistCore.Framework.EService
             return rsp;
         }
 
-        public ISearchResponse<T> Search(Func<SearchDescriptor<T>, ISearchRequest> selector, bool log = false)
+        public async Task<ISearchResponse<T>> SearchAsync(Func<SearchDescriptor<T>, ISearchRequest> selector, bool log = false)
         {
             var index = new T();
             var query = new SearchDescriptor<T>();
@@ -350,7 +281,7 @@ namespace MistCore.Framework.EService
             var query1 = selector(query);
 
             var la = _client.Serializer.SerializeToString(query1, SerializationFormatting.None);
-            var rsp = _client.Search<T>(s => query1);
+            var rsp = await _client.SearchAsync<T>(s => query1);
 
             logger.Log(log ? Microsoft.Extensions.Logging.LogLevel.Information : Microsoft.Extensions.Logging.LogLevel.Debug,
                 $"url: {rsp.ApiCall.Uri.AbsoluteUri}. body: {la}. {rsp.OriginalException}");
@@ -358,12 +289,8 @@ namespace MistCore.Framework.EService
             return rsp;
         }
 
-        public string SerializeToString(SearchDescriptor<T> data, SerializationFormatting formatting = SerializationFormatting.Indented)
-        {
-            return _client.Serializer.SerializeToString(data, formatting);
-        }
-
     }
+
 
 
 }
